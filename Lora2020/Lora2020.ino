ed4a2a7ba70db670de7be5a0661a8ec6e0794c3c
Stephen Cushen <6130247+cushenst@users.noreply.github.com>
2020-03-07 17:20:20 +0000

    added software working

--- Lora2020/Lora2020.ino
+++ Lora2020/Lora2020.ino
@@ -1,8 +1,23 @@
 #include <SPI.h>
 #include <LoRa.h>
+#include <SPI.h>
+#include <LoRa.h>
+#include <Wire.h>  
+#include "SSD1306.h" 
+//#include "images.h"
+
+#define SCK     5    // GPIO5  -- SX1278's SCK
+#define MISO    19   // GPIO19 -- SX1278's MISO
+#define MOSI    27   // GPIO27 -- SX1278's MOSI
+#define SS      18   // GPIO18 -- SX1278's CS
+#define RST     14   // GPIO14 -- SX1278's RESET
+#define DI0     26   // GPIO26 -- SX1278's IRQ(Interrupt Request)
+#define BAND    433E6
+
+SSD1306 display(0x3c, 4, 15);
 
 int counter = 0;
-String id = "~a";
+String id = "~b";
 String message;
 String chk;
 String chk_rm;
@@ -14,18 +29,23 @@ void setup() {
   LoRa.setPins(10,5,3);
   Serial.println("LoRa Sender");
 
-  if (!LoRa.begin(433E6)) {
+  SPI.begin(SCK,MISO,MOSI,SS);
+  LoRa.setPins(SS,RST,DI0);  
+  if (!LoRa.begin(BAND)) {
     Serial.println("Starting LoRa failed!");
-    while (1);
+    while (1){
+      ;
+    }
   }
+  Serial.println("init ok");
 }
 
 void loop() {
   int packetSize = LoRa.parsePacket();
   if (packetSize) {
     // received a packet
-    Serial.print("Received packet '");
     
+    Serial.println("message received");
     // read packet
     while (LoRa.available()) {
       message += (char)LoRa.read();
@@ -35,32 +55,43 @@ void loop() {
       chk_rm = message.substring(message.length()-2, message.length());
       r_message = message.substring(0, message.length()-2);
         if (String(checksum(r_message),HEX) == chk_rm){
-          message += id;
+          r_message += id;
           LoRa.beginPacket();
-          LoRa.print(message);
-          chk = LoRa.print(String(checksum(message), HEX));
+          LoRa.print(r_message);
+          chk = String(checksum(r_message), HEX);
           LoRa.print(chk);
           LoRa.endPacket();
-          Serial.print(message+chk);
+          Serial.println("Starting repeating");
+          Serial.println(r_message+chk);
+          Serial.println("Ending repeating");
+          message = "";
+      }else{
+        Serial.println("chk invalid");
       }
-    }
+    }else{
     
     // print RSSI of packet
+    Serial.println("Start Not repeating");
     Serial.print(message + "' with RSSI ");
     Serial.println(LoRa.packetRssi());
+    Serial.println("End Not repeating");
+    message = "";
+    }
 
     
   }
   if (Serial.available())
   {
     message = Serial.readString();
+    message = message.substring(0, message.length()-1);
     message += id;
     LoRa.beginPacket();
     LoRa.print(message);
-    chk = LoRa.print(String(checksum(message), HEX));
+    chk = String(checksum(message), HEX);
     LoRa.print(chk);
     LoRa.endPacket();
-    Serial.print(message+chk);
+    Serial.println(message+chk);
+    message = "";
   }
   
   
